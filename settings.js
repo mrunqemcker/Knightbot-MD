43f2b0205b6a2ae6a5915f47c35143fd72b9fef6
mrunqemcker <admahmdbdalmnm55@gmail.com>
2025-08-19 22:08:00 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight Bot',
   author: '‎',
-  botName: "VAMPIRE BOT",
-  botOwner: 'JUST B7R', // Your name
-  ownerNumber: '201205817804', //Set your number here without + symbol, just add country code & number without any space
+  botName: "𝑲𝑰𝑳𝑾𝑨 BOT",
+  botOwner: '𝑫𝑬𝑽𝑬𝑳𝑶𝑷𝑬𝑹 𝑩7𝑹', // Your name
+  ownerNumber: '201060391321', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
